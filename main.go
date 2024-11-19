91547b1c9056d9770028ecfe5092093892832db4
ivanday <ivanday9@gmail.com>
2024-11-18 23:29:10 -0600

    actually add api group

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"pick_em_league/handlers"
 )
 
 func main() {
+	router := gin.Default()
 
+	oddsApi := router.Group("/odds")
+	{
+		oddsApi.GET("/", handlers.GetOdds)
+	}
 }
