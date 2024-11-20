aa031513ee74a28ff6388974bb110d48342f10ca
ivanday <ivanday9@gmail.com>
2024-11-19 23:45:59 -0600

    set api key in environment, implement first couple routes for testing

--- handlers/odds.go
+++ handlers/odds.go
@@ -1,7 +1,76 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
 
-func GetOdds(c *gin.Context) {
+var oddsApiKey = os.Getenv("ODDS_API_KEY")
+
+const (
+	NFLUri     = "https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds"
+	collegeUri = "https://api.the-odds-api.com/v4/sports/americanfootball_ncaaf/odds"
+	oddsRoute  = "/odds/"
+	bookMaker  = "draftkings"
+	region     = "us"
+	oddsFormat = "american"
+)
+
+type Spread struct {
+	FavoriteTeam string `json:"favorite_team"`
+	DogTeam      string `json:"dog_team"`
+}
+
+func GetSports(c *gin.Context) {
+	client := http.Client{}
+
+	request, err := http.NewRequest("GET", NFLUri, nil)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "test"})
+	}
+	query := request.URL.Query()
+	query.Add("apiKey", oddsApiKey)
+	query.Add("regions", region)
+	query.Add("oddsFormat", oddsFormat)
+	query.Add("bookmaker", bookMaker)
+	request.URL.RawQuery = query.Encode()
+
+	response, err := client.Do(request)
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "test"})
+	}
+
+	c.JSON(200, string(body))
+}
+
+func GetNFLSpreads(c *gin.Context) {
+	client := http.Client{}
+
+	request, err := http.NewRequest("GET", NFLUri, nil)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "test"})
+	}
+	query := request.URL.Query()
+	query.Add("apiKey", oddsApiKey)
+	query.Add("regions", region)
+	query.Add("oddsFormat", oddsFormat)
+	query.Add("bookmaker", bookMaker)
+
+	request.URL.RawQuery = query.Encode()
+
+	response, err := client.Do(request)
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "test"})
+	}
+
+	c.JSON(200, string(body))
 
 }
--- main.go
+++ main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"pick_em_league/handlers"
 )
 
 func main() {
 	router := gin.Default()
 
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
 	oddsApi := router.Group("/odds")
 	{
-		oddsApi.GET("/", handlers.GetOdds)
+		oddsApi.GET("/sports", handlers.GetSports)
+		oddsApi.GET("/spreads/nfl", handlers.GetNFLSpreads)
+	}
+
+	err = router.Run(":8080")
+	if err != nil {
+		panic(err)
 	}
 }
